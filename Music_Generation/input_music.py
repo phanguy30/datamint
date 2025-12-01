62b0ad291bcc323795c3ac18d411bd44bc91262c
Preethi <preethi.vez@gmail.com>
2025-12-01 00:28:28 -0800

    started the training of the music generation

--- Music_Generation/input_music.py
+++ Music_Generation/input_music.py
@@ -1,14 +1,7 @@
 from mido import MidiFile, MidiTrack
 
 def get_midi_file_notes(filename):
-    """Returns the sequence of notes played in the midi file
-    There are 128 possible notes on a MIDI device, and they are numbered 0 to 127.
-    The middle C is note number 60. Larger numbers indiciate higher pitch notes,
-    and lower numbers indicate lower pitch notes.
-
-    You can read more about the midi representation below, but it is not
-    necessary for this assignment.
-    http://midi.teragonaudio.com/tech/midispec/noteon.htm
+    """Returns notes based on the midi file
     """
     notes = []
     for msg in  MidiFile(filename):
---
+++ Music_Generation/train_music.py
@@ -0,0 +1,4 @@
+from Trainer import trainer
+
+class music_trainer():
+    def __init__(self,notes_,input_len=8,hidden_layer=None,loss_fn=None,optimizer_cls=SGD):
