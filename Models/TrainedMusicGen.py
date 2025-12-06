9473a547d327259908b7c81f07874246bd8c63fa
phan.nguyen <phan.nguyen@mail.utoronto.ca>
2025-12-05 20:33:56 -0800

    adding pretrained model

---
+++ Models/TrainedMusicGen.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Models.MLPMusicGen import MLPMusicGen
+
+class TrainedMusicGen(MLPMusicGen):
+    def __init__(self, model_path = "Models/param.txt"):
+        super().__init__(context_length=20, hidden_sizes= [100], activation_type="relu")
+        self.load_model(model_path)
+    
+    def load_model(self, model_path ):
+        curr_param = self.parameters()
+
+        with open(model_path, "r") as f:
+            pre_trained_params = [float(line.strip()) for line in f]
+        try: 
+            for i, p in enumerate(curr_param):
+                p.data = pre_trained_params[i]
+        except Exception as e:
+            print(len(pre_trained_params), len(list(curr_param)))
+        else: 
+            print("Model parameters loaded successfully.")
+
+if __name__ == "__main__":
+    model = TrainedMusicGen("Models/param.txt")
+    
+    
